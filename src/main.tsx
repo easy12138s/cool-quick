b6da16601561546679ce804aad905043d7cc14af
CoolQuick Developer <developer@coolquick.app>
2026-02-05 10:46:23 +0800

    debug: 添加调试信息排查白屏问题

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Debug: 检查 DOM 是否加载
+console.log('main.tsx loaded')
+console.log('root element:', document.getElementById('root'))
+
+const root = document.getElementById('root')
+if (!root) {
+  console.error('Root element not found!')
+} else {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+  console.log('React app rendered')
+}
